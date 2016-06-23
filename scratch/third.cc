89437500d2e1e427077f0c54e8b3a412f1b421e8
LongLian <18200128263@163.com>
2016-06-24 01:05:47 +0800

    add a file

---
+++ scratch/third.cc
@@ -0,0 +1,176 @@
+#include "ns3/core-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+
+// Default Network Topology
+//
+//   Wifi 10.1.2.0
+//                 AP
+//  *    *    *    *
+//  |    |    |    |    
+
+//  n4  n5   n6    n0   n1   n2   n3 ---server  
+//                 |    |    |    |
+//                 ================
+//                    LAN 10.1.1.0
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("Third_project_1_ScriptExample");
+
+int 
+main (int argc, char *argv[])
+{
+  bool verbose = true;
+  uint32_t nCsma = 4;
+  uint32_t nWifi = 3;
+
+  CommandLine cmd;
+  cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
+  cmd.AddValue ("nWifi", "Number of wifi STA devices", nWifi);
+  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+
+  cmd.Parse (argc,argv);
+
+  if (nWifi > 250)
+    {
+      std::cout << "Number of wifi nodes " << nWifi << 
+                   " specified exceeds the mobility bounding box" << std::endl;
+      exit (1);
+    }
+
+  if (verbose)
+    {
+      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
+
+  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  NodeContainer csmaNodes;
+  csmaNodes.Create (nCsma);
+
+  CsmaHelper csma;
+  csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
+  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+
+  NetDeviceContainer csmaDevices;
+  csmaDevices = csma.Install (csmaNodes);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  NodeContainer wifiStaNodes;
+  wifiStaNodes.Create (nWifi);
+  NodeContainer wifiApNode = csmaNodes.Get (0);
+
+  YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+  YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
+  phy.SetChannel (channel.Create ());
+
+  WifiHelper wifi = WifiHelper::Default ();
+  wifi.SetRemoteStationManager ("ns3::AarfWifiManager");
+
+  NqosWifiMacHelper mac = NqosWifiMacHelper::Default ();
+
+  Ssid ssid = Ssid ("ns-3-ssid");
+  mac.SetType ("ns3::StaWifiMac",
+               "Ssid", SsidValue (ssid),
+               "ActiveProbing", BooleanValue (false));
+
+  NetDeviceContainer staDevices;
+  staDevices = wifi.Install (phy, mac, wifiStaNodes);
+
+  mac.SetType ("ns3::ApWifiMac",
+               "Ssid", SsidValue (ssid));
+
+  NetDeviceContainer apDevices;
+  apDevices = wifi.Install (phy, mac, wifiApNode);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  MobilityHelper mobility;
+
+  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+                                 "MinX", DoubleValue (0.0),
+                                 "MinY", DoubleValue (0.0),
+                                 "DeltaX", DoubleValue (5.0),
+                                 "DeltaY", DoubleValue (10.0),
+                                 "GridWidth", UintegerValue (3),
+                                 "LayoutType", StringValue ("RowFirst"));
+  //STA move as constant velocity.
+  mobility.SetMobilityModel ("ns3::ConstantVelocityMobilityModel");
+  mobility.Install (wifiStaNodes);
+
+  //AP don't move.
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (wifiApNode);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  InternetStackHelper stack;
+  stack.Install (csmaNodes);
+  stack.Install (wifiStaNodes);
+
+  Ipv4AddressHelper address;
+
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer csmaInterfaces;
+  csmaInterfaces = address.Assign (csmaDevices);
+
+  address.SetBase ("10.1.2.0", "255.255.255.0");
+  address.Assign (staDevices);
+  address.Assign (apDevices);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  UdpEchoServerHelper echoServer (9);
+
+  ApplicationContainer serverApps = echoServer.Install (csmaNodes.Get (nCsma-1));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+
+//****************************************************************************
+  UdpEchoClientHelper echoClient (csmaInterfaces.GetAddress (nCsma-1), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+  ApplicationContainer clientApps = 
+    echoClient.Install (wifiStaNodes.Get (nWifi - 1));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+
+  ApplicationContainer clientApps2 = 
+    echoClient.Install (wifiStaNodes.Get (nWifi - 2));
+  clientApps2.Start (Seconds (4.0));
+  clientApps2.Stop (Seconds (10.0));
+
+
+ApplicationContainer clientApps3 = 
+    echoClient.Install (csmaNodes.Get (1));
+  clientApps3.Start (Seconds (6.0));
+  clientApps3.Stop (Seconds (10.0));
+
+ApplicationContainer clientApps4 = 
+    echoClient.Install (csmaNodes.Get (2));
+  clientApps4.Start (Seconds (8.0));
+  clientApps4.Stop (Seconds (10.0));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  Simulator::Stop (Seconds (10.0));
+
+  phy.EnablePcap ("third", apDevices.Get (0));
+  phy.EnablePcap ("third", staDevices.Get (nWifi-1));
+  csma.EnablePcap ("third", csmaDevices.Get (0), true);
+  csma.EnablePcap ("third", csmaDevices.Get (nCsma-1), true);
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+}
